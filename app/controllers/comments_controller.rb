1fba0ac363729571703713f7210c164aeb56b7d1
francksefu <francksefu1998@gmail.com>
2024-03-26 14:28:44 +0200

    Add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -46,6 +46,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.require(:comment).permit(:content)
+      params.require(:comment).permit(:content, :name, :email, :post_id)
     end
 end
